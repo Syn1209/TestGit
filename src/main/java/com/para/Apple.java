6ff183161d3299c5d9781580c80b539abcdbc3e7
syn_glb <TopainTogain_glb@_effort.com>
2019-01-06 17:23:55 +0800

    My first commit in Eclipse

---
+++ src/main/java/com/para/Apple.java
@@ -0,0 +1,7 @@
+package com.para;
+
+public class Apple{
+   public static void main(String[] args) {
+	System.out.println("an apple");
+}
+}
---
+++ src/test/java/com/paratest/appleTest.java
@@ -0,0 +1,13 @@
+package com.paratest;
+
+import org.junit.Test;
+
+/** * @author  作者 E-mail: * @date 创建时间：2019年1月6日 下午4:48:25 * @version 1.0 * @parameter  * @since  * @return  */
+public class appleTest {
+
+	@Test
+	public void test() {
+	//	fail("Not yet implemented");
+	}
+
+}
