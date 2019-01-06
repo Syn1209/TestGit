79590fc92055bf4b8d3d0b34b34c31673e104ddd
syn_glb <TopainTogain_glb@_effort.com>
2019-01-06 17:28:25 +0800

    new JavaFile

---
+++ src/main/java/com/para/Happy.java
@@ -0,0 +1,15 @@
+package com.para;
+/**   @author  作者 E-mail:  
+ *    @date 创建时间：2019年1月6日 下午5:24:54
+ *    @version 1.0 
+ *    @parameter  
+ *    @since  
+ *    @return  */
+class Happy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
